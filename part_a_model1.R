a7014bec30c8921ec3a629fa6a568f685c20e9f4
Idriss Gackou <rgackou@hotmail.co.uk>
2018-05-17 15:28:58 -0400

    saving part_a_model1.R

--- part_a_model1.R
+++ part_a_model1.R
@@ -19,7 +19,31 @@ write.csv(df, file = 'part_a_model1_output.csv')
 
 #Build a linear model, trying to predict ltr based on the hotel condition
 
+hotelData <- cleanData
 
+modelA1 <- lm(formula = hotelData$Condition_Hotel_H ~ hotelData$Likelihood_Recommend_H, data= hotelData)
+
+plot(hotelData$Condition_Hotel_H,hotelData$Likelihood_Recommend_H)
+
+abline(modelA1)
+
+
+#Build a linear model that predicts ltr based on the hotel condition and the staff_cared attributes
+
+
+  
+staffPerCond <- hotelData$Staff_Cared_H / hotelData$Condition_Hotel_H
+staffPerCond <- round(staffPerCond, digits=2)
+
+staffPerCond <- data.frame(staffPerCond) 
+newHotelData <- cbind(hotelData, staffPerCond)
+
+
+modelA2 <- lm(formula = staffPerCond ~ hotelData$Likelihood_Recommend_H, data= newHotelData)
+
+plot(staffPerCond,hotelData$Likelihood_Recommend_H )
+
+abline(modelA2)
 
 
 
