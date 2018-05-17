f05b65f2519640c50d912041bc88817b14ec40f3
Idriss Gackou <rgackou@hotmail.co.uk>
2018-05-17 16:17:33 -0400

    saving part_a_model1.R

--- part_a_model1.R
+++ part_a_model1.R
@@ -31,14 +31,17 @@ summary(modelA1)
 #Build a linear model that predicts ltr based on the hotel condition and the staff_cared attributes
 
 
-  
-staffPerCond <- hotelData$Staff_Cared_H / hotelData$Condition_Hotel_H
+# creates a number based on the ratio between staff care and the hotel condition
+staffPerCond <- hotelData$Staff_Cared_H / hotelData$Condition_Hotel_H 
+# rounds that number
 staffPerCond <- round(staffPerCond, digits=2)
 
+# puts the numbere into a new data frame
 staffPerCond <- data.frame(staffPerCond) 
+# combines the new data frame with the hotelData to create a new hotelData dataframe with my new number
 newHotelData <- cbind(hotelData, staffPerCond)
 
-
+#generating the linear regression formula 
 modelA2 <- lm(formula = Likelihood_Recommend_H  ~ staffPerCond, data= newHotelData)
 
 plot(newHotelData$staffPerCond,newHotelData$Likelihood_Recommend_H )
