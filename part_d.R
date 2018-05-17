12721d25b4a64ce974e9e831b6f6071799a8d439
alexbruso <39306864+alexbruso@users.noreply.github.com>
2018-05-17 16:43:17 -0400

    saving part_d.R

--- part_d.R
+++ part_d.R
@@ -9,7 +9,13 @@ df <- read.csv('part_C_output.csv')
 ####################################
 
 ## start writing your R code from here
+svmOutput2 <- ksvm(isDetractor~Condition_Hotel_H+Staff_Cared_H, data=trainData, C=5, prob.model=TRUE)
 
+svmPred2 <- predict(svmOutput2, testData[1650,], type="votes")
+compTable2 <- data.frame(testData[1650,60],svmPred2[1,])
+
+sample1 <- replicate(table(compTable2),1000)
+sample1
 ## end your R code and logic 
 
 ####################################
